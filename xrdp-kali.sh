efb5a415f4d9c3ce899886c6bfe86dd5e3967834
hackerfromprishtina <149076676+hackerfromprishtina@users.noreply.github.com>
2023-10-27 23:04:30 +0200

    Update xrdp-kali.sh

--- xrdp-kali.sh
+++ xrdp-kali.sh
@@ -29,7 +29,8 @@ echo "===================================="
 echo "Username : ubuntu"
 echo "Password : ubuntu"
 echo "VNC Address:"
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' | sed 's/0.tcp/6.tcp'  # Modify ngrok URL
+VNC_ADDR=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' | sed 's/0.tcp/6.tcp')  # Modify ngrok URL
+echo "$VNC_ADDR"
 echo "IP:" && curl --silent --show-error ipconfig.io
 echo "===================================="
 echo "===================================="
